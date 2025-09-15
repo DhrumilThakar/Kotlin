3b736107376533f3e3e902f0f998a45a6ee07e89
Dhrumil Thakar <dhruvmilthakar>
2025-09-16 01:30:36 +0530

    about data type

---
+++ DataType.kt
@@ -0,0 +1,19 @@
+package main.kotlin
+
+fun main()
+{
+    var integerMaxValue = Int.MAX_VALUE
+    var integerMinValue = Int.MIN_VALUE
+    println("The maximum value of integer Data type is :$integerMaxValue")
+    println("The Minimum value of Integer Data type is :$integerMinValue")
+
+    var byteMaxvalue = Byte.MAX_VALUE
+    var byteMinvalue = Byte.MIN_VALUE
+    println("The maximum value of Byte Data type is :$byteMaxvalue")
+    println("The minimum value of Byte Data type is :$byteMinvalue")
+
+    var shortMaxValue = Short.MAX_VALUE
+    var shortMinValue = Short.MIN_VALUE
+    println("The maximum value of short data type is :$shortMaxValue")
+    println("The minimum value of short data type is :$shortMinValue")
+}
