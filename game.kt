f39b4754a64c5a8c93e853a4af0c2f91fe6f73cc
Dhrumil Thakar <dhruvmilthakar@gmail.com>
2026-03-03 19:27:12 +0530

    Add notes on for loop

--- game.kt
+++ game.kt
@@ -1,7 +1,19 @@
-fun getGameChoice(optionsParam: Array<String>):Int{
+fun getGameChoice(optionsParam: Array<String>){
     optionsParam[(Math.random()*optionsParam.size).toInt()]
-
 }
+
+fun getUserChoice(otionsParam:Array<String>): String{
+    println("Please Enter One of the Following : Rock, paper, Scissor :")
+}
+
+/*For Loop
+For loop is a specail type of loop in which we specfi the range we want to run that loop syntax for(var_name in starting_point .. ending point) {//code}
+
+->IF we don't want to include the ending point we can use until instead of ..
+->If we want to increment the in any order we can use step like for(x in 1..100 step 2) {//code}
+->If we want to travel in reverse order than we can use downto (i.e. for(x in 100 downto 1) {//code}
+->IF we want to travel in an array we can write for(var_name in array_name){//code}
+ */
 
 fun main(){
 
